2277988341e912518671fff8aeada0b764703116
Amrane Noureddine <amranenoureddin3@gmail.com>
2019-01-17 12:37:09 +0100

    add selector

--- src/Server.java
+++ src/Server.java
@@ -3,10 +3,7 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 
 public class Server {
@@ -19,12 +16,13 @@ public class Server {
         selector = Selector.open();
         SocketAddress  socketAddress = new InetSocketAddress(2020);
         serverSocketChannel.bind(socketAddress);
+        serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
         run();
 
     }
 
-    public void run() throws IOException {
+    public void runS() throws IOException {
         SocketChannel socketChannel = serverSocketChannel.accept();
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(512);
         while (socketChannel.isConnected()){
@@ -34,6 +32,33 @@ public class Server {
             CharBuffer charBuffer = charset.decode(byteBuffer);
             System.out.println(charBuffer);
             byteBuffer.clear();
+
+        }
+    }
+    public void run() throws IOException{
+        while (true){
+            selector.select();
+            for(SelectionKey selectionKey: selector.selectedKeys() ){
+                if (selectionKey.isAcceptable()){
+                    SelectableChannel selectableChannel = selectionKey.channel();
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectableChannel;
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector,SelectionKey.OP_READ);
+                }else{
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(512);
+                    socketChannel.read(byteBuffer);
+                    byteBuffer.flip();
+                    Charset charset = Charset.forName("UTF-8");
+                    CharBuffer charBuffer = charset.decode(byteBuffer);
+                    System.out.println(charBuffer);
+                    byteBuffer.clear();
+                }
+
+            }
+            //Clear selector
+            selector.selectedKeys().clear();
 
         }
     }
